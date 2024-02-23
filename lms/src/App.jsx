e5f5d4c4dbd5da4c2db65d9b1193acf44b97ffef
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-02-23 20:14:57 +0530

    plane project

---
+++ lms/src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center'>React Calculator</h1>
+    </>
+  )
+}
+
+export default App
